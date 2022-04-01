7b4bc756f34347c137d5e95fee73128ed6906ceb
myself-pabitra <75253329+myself-pabitra@users.noreply.github.com>
2022-04-01 17:55:26 +0530

    Add files via upload

--- noteapp/urls.py
+++ noteapp/urls.py
@@ -3,6 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home , name='home'),
+    path('logout', views.logout_user, name='logout'),
+    path('login', views.login_user , name='login'),
+    # path('', views.user_register , name='register'),
     path('edit/<str:pk>/', views.edit , name='edit'),
     path('delete/<str:pk>/', views.delete , name='delete'),
 ]
--- noteapp/views.py
+++ noteapp/views.py
@@ -1,37 +1,70 @@
 from asyncio import Task, tasks
+from email import message
 from multiprocessing import context
+from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .models import Notes
-
-# Create your views here.
+from django.contrib.auth import login,logout,authenticate
+from django.contrib import messages
+# from django.contrib.auth.decorators import login_required
 
 def home(request):
-    if request.method == 'POST':
-        input1 = request.POST['title']
-        input2 = request.POST['description']
-        Notes.objects.create(title = input1 , description = input2 )
-        return redirect('home')
-    obj = Notes.objects.all()
-    context = {'tasks':obj}
-    return render(request , 'home.html',context)
+    if request.user.is_authenticated:
+        if request.method == 'POST':
+            input1 = request.POST['title']
+            input2 = request.POST['description']
+            Notes.objects.create(title = input1 , description = input2 )
+            messages.success(request,'Note Added Successfully..')
+            return redirect('home')
+        obj = Notes.objects.all()
+        context = {'tasks':obj , 'login': True}
+        return render(request , 'home.html',context)
+    else:
+        return render(request , 'home.html')
+
 
 def edit(request,pk):
-    note = Notes.objects.get(id=pk)
-    previous_title = note.title
-    prev_description = note.description
-    if request.method == 'POST':
-        new_title = request.POST['title']
-        new_description = request.POST['description']
-        note.title = new_title
-        note.description = new_description
-        note.save()
-        return redirect('home')
-    obj = Notes.objects.all()
-    context = {'tasks':obj , 'title':previous_title , 'description': prev_description}
-    return render(request,'home.html',context)
+    if request.user.is_authenticated:
+        note = Notes.objects.get(id=pk)
+        previous_title = note.title
+        prev_description = note.description
+        if request.method == 'POST':
+            new_title = request.POST['title']
+            new_description = request.POST['description']
+            note.title = new_title
+            note.description = new_description
+            note.save()
+            return redirect('home')
+        obj = Notes.objects.all()
+        context = {'tasks':obj , 'title':previous_title , 'description': prev_description}
+        return render(request,'home.html',context)
+    else:
+        return redirect('login')
+
 
 def delete(request,pk):
-    # if request.method == 'POST':
-        note = Notes.objects.get(id = pk)
-        note.delete()
-        return redirect('home')
+    if request.user.is_authenticated:
+        # if request.method == 'POST':
+            note = Notes.objects.get(id = pk)
+            note.delete()
+            return redirect('home')
+    else:
+        return redirect('login')
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['pswd']
+        user = authenticate(username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'Login successful')
+            return redirect('home')
+
+        else:
+            messages.error(request,'Username or Password is Incorrect. Please give coreect one')
+    return render(request,'login.html')
